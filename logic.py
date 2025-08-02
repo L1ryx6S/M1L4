e23e14a42c52411a751130b8f5c9dcbf9dbe4758
Михаил <mixa.biryukov11@gmail.com>
2025-08-02 11:03:21 +0300

    omam

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 from random import randint
 import requests
-
+from datetime import datetime, timedelta
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
@@ -13,6 +13,7 @@ class Pokemon:
 
         self.hp = randint(200, 400)
         self.power = randint(30, 60)
+        self.last_feed_time = datetime.now()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -62,9 +63,21 @@ class Pokemon:
         else:
             enemy.hp = 0
             return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
-        
+
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {self.last_feed_time+delta_time}"
+
 class Wizard(Pokemon):
-    pass
+    def feed(self):
+        return super().feed(hp_increase=20)
+    
 class Fighter(Pokemon):
     def attack(self, enemy):
         super_power = randint(5, 15)
@@ -73,7 +86,9 @@ class Fighter(Pokemon):
         self.power -= super_power
         return result + f'\nБоец применил супер-атаку силой {super_power}'
 
-
+    def feed(self):
+        return super().feed(feed_interval=10)
+    
 if __name__ == '__main__':
     wizard = Wizard("username1")
     fighter = Fighter("username2")
